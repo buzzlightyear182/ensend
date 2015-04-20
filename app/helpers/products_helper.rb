ed61418919eecab0e5bf1d7a3116b4f6e7b010ef
buzzlightyear182 <jane.buzzlightyear@gmail.com>
2015-04-21 01:55:59 +0800

    Fixing JS

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,12 @@
 module ProductsHelper
+
+  def check_links_of product
+    unless product.links.empty?
+      content_tag :p do
+        link_to 'Apply now', visit_url_path(product.homepage.counter.id), method: 'put', :class => 'btn btn-xl btn-primary'
+      end
+    end
+  end
 end
+# <p>Apply counter: <%= @product.homepage.counter.count %> - <%= @product.affiliate_link.url %></p>
+#       <p>Visit website: <%= @product.affiliate_link.counter.count %> - <%= @product.homepage.url %></>
