1bd5d618cc866b7f169e8209ad20c17320b665df
buzzlightyear182 <jane.buzzlightyear@gmail.com>
2015-04-21 02:34:12 +0800

    Refactor to render

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,11 +2,7 @@ module ProductsHelper
 
   def check_links_of product
     unless product.links.empty?
-      content_tag :p do
-        link_to 'Apply now', visit_url_path(product.homepage.counter.id), method: 'put', :class => 'btn btn-xl btn-primary'
-      end
+      render partial: 'product_link', object: product
     end
   end
 end
-# <p>Apply counter: <%= @product.homepage.counter.count %> - <%= @product.affiliate_link.url %></p>
-#       <p>Visit website: <%= @product.affiliate_link.counter.count %> - <%= @product.homepage.url %></>
