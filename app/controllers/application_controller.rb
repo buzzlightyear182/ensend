45dd7279ad7b6725b6071ac944fd41607f266d1f
buzzlightyear182 <jane.buzzlightyear@gmail.com>
2015-05-06 11:13:53 +0800

    Removed redirection upon sign-in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def after_sign_in_path_for(resource)
-    if user_signed_in? && current_user.roles == "admin"
-      admin_dashboard_path current_user
-    else
-      products_path
-    end
-  end
-
   def check_page_count product
     if current_user.roles == "admin"
       return
